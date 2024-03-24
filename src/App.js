e6c237e037832cc95f78557076792f46d7c51733
tienthinhit54 <dtthinhit.54@gmail.com>
2024-03-24 23:35:37 +0700

    zingmp3

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import { useState } from "react";
-
-
+import Home from './containers/public/Home';
+import Public from './containers/public/Public';
+import Login from './containers/public/Login';
+import { ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import path from './utils/path';
 
 function App() {
-  const [word, setWord] = useState('');
-  const [todos, setTodos] = useState([]);
-  const handleAdd = ()=>{
-    setTodos(prev =>[...prev, word])
-    setWord('')
-  } 
+
   return (
-    <div className="flex flex-col gap-8 h-screen items-center border border-red-500 justify-center">
-      <div className='flex gap-8'>
-      <input
-        type="text"
-        className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
-        value={word} 
-        onChange={e => setWord(e.target.value)} 
-        ></input>
-        <button
-        type="button"
-        className="outline-none px-4 py-2 bg-blue-500 rounded-md"
-        onClick={handleAdd}
-        >Add</button>
-      </div>
-      <div>
-        <h3 className='font-bold text-xl'>Content: </h3>
-        <ul>
-          {todos?.map((item, index) => {
-            return(
-              <li key={index}>
-                {item}
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+    <>
+    <div className="">
+      <Routes>
+        <Route path={path.PUBLIC} element={<Public/>}>
+          <Route path={path.HOME} element={<Home/>} />
+          <Route path={path.LOGIN} element={<Login/>} /> 
+          
+        </Route>
+      </Routes>
     </div>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+    </>
   );
 }
 
---
+++ src/containers/public/index.js
@@ -0,0 +1,3 @@
+import { Default as Home } from './Home';
+import { Default as Public } from './Public';
+import { Default as Login } from './Login';
